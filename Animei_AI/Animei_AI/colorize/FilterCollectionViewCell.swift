78819e7c0863952f5bf1f4230011097eb74fc63e
Adrian <adrian.z.mei@gmail.com>
2018-04-21 17:32:57 -0700

    project template is ready

---
+++ Animei_AI/Animei_AI/colorize/FilterCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FilterCollectionViewCell.swift
+//  Colorization
+//
+//  Created by Adrian on 4/21/18.
+//  Copyright © 2018 Mei. All rights reserved.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 8
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth =  1
+    }
+}
