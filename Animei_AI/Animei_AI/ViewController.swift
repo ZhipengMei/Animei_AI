42655c7ceb984300d88cadb5b01350bfe202100d
Adrian <adrian.z.mei@gmail.com>
2018-04-21 17:17:36 -0700

    started project

---
+++ Animei_AI/Animei_AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animei_AI
+//
+//  Created by Adrian on 4/21/18.
+//  Copyright © 2018 Mei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    //MARK:- ViewController life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+}
